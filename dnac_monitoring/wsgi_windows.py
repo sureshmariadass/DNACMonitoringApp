e31aad23a7ee446481caaa8836627ceb15f62a48
sureshmariadass <mariadass25@gmail.com>
2021-01-07 13:43:43 +0800

    for jenkins

--- dnac_monitoring/wsgi_windows.py
+++ dnac_monitoring/wsgi_windows.py
@@ -3,14 +3,14 @@ import sys
 import site
 
 # Add the site-packages of the chosen virtualenv to work with
-site.addsitedir("C:/Users/Administrator/Desktop/dnac_monitoring/venv/Lib/site-packages")
+site.addsitedir("C:/Windows/System32/config/systemprofile/AppData/Local/Jenkins/.jenkins/workspace/DNACMonitoring/venv/Lib/site-packages")
 
 
 
 
 # Add the app's directory to the PYTHONPATH
-sys.path.append('C:/Users/Administrator/Desktop/dnac_monitoring')
-sys.path.append('C:/Users/Administrator/Desktop/dnac_monitoring')
+sys.path.append('C:/Windows/System32/config/systemprofile/AppData/Local/Jenkins/.jenkins/workspace/DNACMonitoring')
+sys.path.append('C:/Windows/System32/config/systemprofile/AppData/Local/Jenkins/.jenkins/workspace/DNACMonitoring')
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'dnac_monitoring.settings'
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dnac_monitoring.settings")
