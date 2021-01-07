5cbef8133b7a39a1270f610cfbb428cc223a2e1a
sureshmariadass <mariadass25@gmail.com>
2021-01-07 11:50:00 +0800

    for jenkins

--- deployment/deploy_prod.bat
+++
@@ -1,7 +0,0 @@
-  cd "C:\DNAC monitoring App"
-  git pull
-  "C:\DNAC monitoring App\dnac_monitoring\venv\Scripts\activate.bat"
-  pip install -r requirements.txt
-  ./manage.py migrate
-  httpd -k restart
-  exit
--- dnac_monitoring/settings.py
+++ dnac_monitoring/settings.py
@@ -39,6 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'liveapi',
 ]
+JENKINS_TASKS = (
+    'django_jenkins.tasks.run_pep8',
+    'django_jenkins.tasks.run_pyflakes',
+    'django_jenkins.tasks.run_jslint',
+    'django_jenkins.tasks.run_csslint',
+    'django_jenkins.tasks.run_sloccount'
+)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
