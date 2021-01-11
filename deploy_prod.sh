2b8f8b337c373f595601c4fcf7c19d7c7ab31823
sureshmariadass <mariadass25@gmail.com>
2021-01-11 19:26:12 +0800

    for jenkins

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -5,10 +5,10 @@ sshpass -p C1sc0123 ssh -tt -o StrictHostKeyChecking=no root@10.171.92.112 <<EOF
   git init
   git pull https://github.com/sureshmariadass/DNACMonitoringApp.git
   ls
-  chmod -r 777 /DNAC/
   virtualenv env -p python3
   source ./env/bin/activate
   pip install -r requirements.txt
+  chmod +x manage.py
   ./manage.py migrate
   exit
 EOF
