9fefb8fe3e500f5b0b44b71602e18673de509f0f
sureshmariadass <mariadass25@gmail.com>
2021-01-11 16:47:32 +0800

    for jenkins

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
-cd /DNAC/
-mkdir ss
-git pull
+
+sshpass -p C1sc0123 ssh root@10.171.92.112 <<EOF
+  cd /DNAC/
+  git pull
+  virtualenv env -p python3
+  source ./env/bin/activate
+  pip install -r requirements.txt
+  ./manage.py migrate
+  exit
+EOF
