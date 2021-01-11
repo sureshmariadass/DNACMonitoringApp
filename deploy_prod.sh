aeeff16caede9ab4f5cf4572b2f8dd1dd28153f1
sureshmariadass <mariadass25@gmail.com>
2021-01-11 19:06:13 +0800

    for jenkins

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -2,6 +2,7 @@
 
 sshpass -p C1sc0123 ssh -tt -o StrictHostKeyChecking=no root@10.171.92.112 <<EOF
   cd /DNAC/
+  git init
   git pull https://github.com/sureshmariadass/DNACMonitoringApp.git
   virtualenv env -p python3
   source ./env/bin/activate
